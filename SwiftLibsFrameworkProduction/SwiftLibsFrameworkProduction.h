9b39773cbb3982277d2ee20708e260b28ce55028
Josh Green <jgreen@pillartechnology.com>
2016-01-15 10:45:42 -0500

    Added Production-Framework.

---
+++ SwiftLibsFrameworkProduction/SwiftLibsFrameworkProduction.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLibsFrameworkProduction.h
+//  SwiftLibsFrameworkProduction
+//
+//  Created by Josh Green on 1/15/16.
+//  Copyright © 2016 Pillar Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLibsFrameworkProduction.
+FOUNDATION_EXPORT double SwiftLibsFrameworkProductionVersionNumber;
+
+//! Project version string for SwiftLibsFrameworkProduction.
+FOUNDATION_EXPORT const unsigned char SwiftLibsFrameworkProductionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLibsFrameworkProduction/PublicHeader.h>
+
+
