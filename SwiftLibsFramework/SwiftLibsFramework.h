1ca56925207a30ec0975952f757f803e82d1f596
Josh Green <jgreen@pillartechnology.com>
2016-01-15 10:06:35 -0500

    Added framework-build script.

---
+++ SwiftLibsFramework/SwiftLibsFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLibsFramework.h
+//  SwiftLibsFramework
+//
+//  Created by Josh Green on 1/15/16.
+//  Copyright © 2016 Pillar Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLibsFramework.
+FOUNDATION_EXPORT double SwiftLibsFrameworkVersionNumber;
+
+//! Project version string for SwiftLibsFramework.
+FOUNDATION_EXPORT const unsigned char SwiftLibsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLibsFramework/PublicHeader.h>
+
+
