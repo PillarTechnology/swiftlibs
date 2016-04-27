b990e01cda6a8d6b5cfd122d9bcf66d187ff67ae
John F.P. Dazey <jdazey@pillartechnology.com>
2016-04-27 12:29:44 -0400

    Escaping spaces in setUrl so execute does not crash

--- SwiftLibsTests/WebRequestTests.swift
+++ SwiftLibsTests/WebRequestTests.swift
@@ -415,4 +415,26 @@ class WebRequestTests: XCTestCase {
         // Assert
         XCTAssertEqual(header, expectedHeader)
     }
+
+    func testSetUrlDoesNotEscapeCharactersRequiredInRootOfUrl() {
+        // Setup
+        let expectedUrl = "https://www.potter.com:1234/index.badphp"
+
+        // Action
+        _subject.setUrl(expectedUrl)
+
+        // Assert
+        XCTAssertEqual(_subject._url, expectedUrl)
+    }
+
+    func testSetUrlEscapesSpacesInRootOfUrl() {
+        // Setup
+        let expectedUrl = "spaced%20out"
+
+        // Action
+        _subject.setUrl("spaced out")
+
+        // Assert
+        XCTAssertEqual(_subject._url, expectedUrl)
+    }
 }
